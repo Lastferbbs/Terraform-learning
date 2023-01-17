0085db92c22252c32cf4bc9b9be084db1fd72765
Lastferbbs <lolpol997@gmail.com>
2023-01-18 00:11:54 +0100

    Added web servers cluster with load balancer

---
+++ Terraform_Up_and_Running_exercises/exercise_2/main.tf
@@ -0,0 +1,111 @@
+provider "aws" {
+  region = "us-east-2"
+
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id               = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+ 
+  #Required when using a launch config with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+
+
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+data "aws_subnets" "default" {
+   filter {
+     name    = "vpc-id"
+     values  = [data.aws_vpc.default.id]
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE AUTOSCALLING GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids  
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+     key                             = "Name"
+     value                           = "terraform-asg-example"
+     propagate_at_launch             = true
+    }
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port 
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+   name = "terraform-example-alb"
+
+   # Allow inbound HTTP requests
+   ingress {
+     from_port      = 80
+     to_port        = 80
+     protocol       = "tcp"
+     cidr_blocks    = ["0.0.0.0/0"]
+
+
+   # Allow all outbound requests
+   ingress {
+     from_port      = 0
+     to_port        = 0
+     protocol       = "-1"
+     cidr_blocks    = ["0.0.0.0/0"]
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
